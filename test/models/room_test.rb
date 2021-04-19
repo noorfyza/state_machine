4c0f507301e7c46c2fbccaf6ae39fb5ffde10293
noorfyza <faizza.rk26@gmail.com>
2021-04-20 00:13:38 +0530

    Added model test

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid room' do
+    room = Room.new(person_name: "John Doe", no_of_romms: 1, amount_per_room: 1200)
+    assert room.valid?
+  end
+
+  test 'valid room with person address' do
+    room = Room.new(person_name: "Angel Priya", person_address: "123 Avenue NYC", no_of_romms: 12, amount_per_room: 1200)
+    assert room.valid?
+  end
 end
